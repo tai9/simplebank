9167c2bf79839c0e611385d3b5410ccffee1a305
Tai <tantai.it95@gmail.com>
2022-03-09 14:30:31 +0700

    implement RESTful api

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/tai9/simplebank/api"
+	db "github.com/tai9/simplebank/db/sqlc"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	address  = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+	err = server.Start(address)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+
+}
